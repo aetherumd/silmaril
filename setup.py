62921e763ad56d9c0c0e522b385224237c43f2b6
Seyong <seyongpark2001@gmail.com>
2023-11-02 15:02:01 -0400

    added boilerplate

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name="silmaril",
+    version="0.0.1",
+    author="Seyong Park",
+    description="Library for generating synthetic observations of strongly lensed high-redshift galaxies",
+    url="https://github.com/syp2001/silmaril",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+    ),
+    install_requires=["scipy","numpy","matplotlib","astropy","tqdm"]
+)
