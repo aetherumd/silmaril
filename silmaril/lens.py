5e051319e79cf1f605e898aee085265c5643915b
Seyong <seyongpark2001@gmail.com>
2023-11-08 19:14:08 -0500

    magnification and convergence

--- silmaril/lens.py
+++ silmaril/lens.py
@@ -1,13 +1,61 @@
+from astropy.wcs.utils import proj_plane_pixel_scales
+from astropy.cosmology import FlatLambdaCDM
+from scipy.ndimage import map_coordinates
+from utilities import *
 
 class Lens():
-    def __init__(self,x_deflections,y_deflections,wcs,redshift):
-        pass
+    """
+    Class representing a lensing cluster.
+
+    :param x_deflections: x-component of the deflection angles
+    :type x_deflections: np.ndarray
+    :param y_deflections: y-component of the deflection angles
+    :type y_deflections: np.ndarray
+    :param wcs: WCS object for the deflection angle map
+    :type wcs: astropy.wcs.WCS
+    :param redshift: redshift of the lensing cluster
+    :type redshift: float
+    :unit: units of the deflection angles (options are "arcseconds" and "pixels"), defaults to "arcseconds"
+    :type unit: str, optional
+    """
+    def __init__(self,x_deflections,y_deflections,wcs,redshift,unit="arcsec"):
+        self.wcs = wcs
+        self.redshift = redshift
+        self.scale = proj_plane_pixel_scales(wcs)*3600
+        if unit == "arcsec":
+            self.x_deflections, self.y_deflections = x_deflections/self.scale[0], y_deflections/self.scale[1]
+        elif unit == "pixels":
+            self.x_deflections, self.y_deflections = x_deflections, y_deflections
+        else:
+            raise ValueError("unit must be either 'arcsec' or 'pixels'")
     
     def magnification(self,grid,source_redshift):
-        pass
+        
+        reshaped_grid = np.transpose(a=grid,axes=(1,2,0))
+        y = reshaped_grid[:,0,1]
+        x = reshaped_grid[0,:,0]
+        traced_points_x, traced_points_y = self.trace_grid(grid,source_redshift)
 
-    def convergence(self,grid):
-        pass
+        # compute jacobian
+        dxdx = np.gradient(traced_points_x, x, axis=1)
+        dxdy = np.gradient(traced_points_x, y, axis=0)
+        dydx = np.gradient(traced_points_y, x, axis=1)
+        dydy = np.gradient(traced_points_y, y, axis=0)
+
+        return abs(1/(dxdx*dydy-dxdy*dydx))
+
+    def convergence(self,grid,source_redshift):
+
+        reshaped_grid = np.transpose(a=grid,axes=(1,2,0))
+        y = reshaped_grid[:,0,1]
+        x = reshaped_grid[0,:,0]
+        traced_points_x, traced_points_y = self.trace_grid(grid,source_redshift)
+
+        # compute jacobian
+        dxdx = np.gradient(traced_points_x, x, axis=1)
+        dydy = np.gradient(traced_points_y, y, axis=0)
+
+        return 1-0.5*(dxdx+dydy)
 
     def magnification_line(self,grid,source_redshift,threshold=500):
         pass
@@ -15,8 +63,45 @@ class Lens():
     def caustic(self,image_plane_grid,source_redshift,threshold=500):
         pass
 
-    def trace_grid(self,grid):
+    def trace_grid(self,grid,source_redshift):
+        # compute deflection angle scale factor
+        scale_factor = deflection_angle_scale_factor(self.redshift,source_redshift)
+
+        # convert coordinate grids into list of points
+        image_plane_points_world = list_of_points_from_grid(grid)
+        image_plane_points_pix = self.wcs.all_world2pix(image_plane_points_world,0,ra_dec_order=True)
+
+        # perform ray tracing by interpolating deflection angles
+        traced_points_x = image_plane_points_pix[:,0] - scale_factor*map_coordinates(self.x_deflections, np.flip(image_plane_points_pix.T,axis=0), order=1)
+        traced_points_y = image_plane_points_pix[:,1] - scale_factor*map_coordinates(self.y_deflections, np.flip(image_plane_points_pix.T,axis=0), order=1)
+
+        traced_points_x, traced_points_y = self.wcs.all_pix2world(traced_points_x,traced_points_y,0,ra_dec_order=True)
+
+        n = grid[0].shape[0]
+        traced_points_x = np.reshape(traced_points_x, (n,-1))
+        traced_points_y = np.reshape(traced_points_y, (n,-1))
+
+        return traced_points_x, traced_points_y
+
+    def trace_points(self,points,source_redshift):
         pass
 
-    def trace_points(self,points):
-        pass
+
+def deflection_angle_scale_factor(z1,z2,H0=70,Om0=0.3):
+    """
+    Compute the deflection angle scale factor :math:`D_{ds}/D_s` for a lens at redshift z1 and a source at redshift z2.
+
+    :param z1: redshift of the lens
+    :type z1: float
+    :param z2: redshift of the source
+    :type z2: float
+
+    :return: deflection angle scale factor :math:`D_{ds}/D_s`
+    :rtype: float
+    """
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+
+    D_ds = cosmo.angular_diameter_distance_z1z2(z1,z2) # distance from source to lens
+    D_s = cosmo.angular_diameter_distance_z1z2(0,z2) # distance to source
+
+    return float(D_ds/D_s)
---
+++ silmaril/utilities.py
@@ -0,0 +1,90 @@
+import numpy as np
+from scipy.ndimage import map_coordinates, rotate, shift
+from astropy.io import fits
+
+def open_fits(file_name):
+    return fits.open(file_name)[0].data
+
+def transform_image(image,angle,center,pixel_scale):
+    """
+    Rotates and shifts an image.
+
+    :param image: image to transform
+    :type image: numpy.ndarray
+    :param angle: angle by which to rotate the image given in degrees
+    :type angle: float
+    :param center: new center of the image
+    :type center: tuple
+    :param pixel_scale: pixel scale of the image
+    :type pixel_scale: float
+
+    :return: transformed image
+    :rtype: numpy.ndarray
+    """
+    pixel_shift = -center[1]/pixel_scale, center[0]/pixel_scale
+    transformed_image = image
+    transformed_image = rotate(transformed_image,angle,order=0,reshape=False)
+    transformed_image = shift(transformed_image,pixel_shift,order=0)    
+    # return al.Array2D.no_mask(transformed_image,pixel_scales=scale)
+    return transformed_image
+
+def fast_griddata(points,values,xi):
+    """
+    A faster version of scipy.interpolate.griddata implemented using scipy.ndimage.map_coordinates.
+
+    :param points: list of points at which the values are defined
+    :type points: numpy.ndarray
+    :param values: values to interpolate
+    :type values: numpy.ndarray
+    :param xi: list of points at which to interpolate
+    :type xi: numpy.ndarray
+
+    :return: interpolated values
+    :rtype: numpy.ndarray
+    """
+    n = np.sqrt(points.shape[0]).astype(int)
+    y_min, y_max = np.min(points[:,1]), np.max(points[:,1])
+    x_min, x_max = np.min(points[:,0]), np.max(points[:,0])
+    i = (y_max-xi[:,1])/(y_max-y_min)*(n-1)
+    j = (xi[:,0]-x_min)/(x_max-x_min)*(n-1)
+
+    values_2d = np.reshape(values, (n,n))
+    return map_coordinates(values_2d, np.array([i, j]), order=1)
+
+def list_of_points_from_grid(grid):
+    """
+    Flattens a grid into a list of points.
+
+    :param grid: grid to flatten
+    :type grid: numpy.ndarray
+
+    :return: flattened grid
+    :rtype: numpy.ndarray
+    """
+    grid_points = np.transpose(grid,(1,2,0))
+    grid_points = np.reshape(grid_points,(-1,2))
+
+    return grid_points
+
+
+def grid(center,num_pix,pixel_scale):
+    """
+    Create a 2D grid of coordinates.
+
+    :param center: center of the grid
+    :type center: tuple
+    :param fov: field of view of the grid
+    :type fov: float
+    :param pixel_scale: pixel scale of the grid
+    :type pixel_scale: float
+
+    :return: 2D grid of coordinates
+    :rtype: numpy.ndarray
+    """
+    x_center = center[0]
+    y_center = center[1]
+    fov = num_pix*pixel_scale
+    x = np.linspace(x_center + fov / 2 - pixel_scale / 2, x_center - fov / 2 + pixel_scale / 2, int(fov / pixel_scale))
+    y = np.linspace(y_center - fov / 2 + pixel_scale / 2 , y_center + fov / 2 - pixel_scale / 2, int(fov / pixel_scale))
+    return np.meshgrid(x, y)
+
