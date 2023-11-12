f061264018d26dc0fb246b992eb588e4d71d4925
Seyong <seyongpark2001@gmail.com>
2023-11-12 16:13:45 -0500

    added grid with wcs

--- silmaril/imaging.py
+++ silmaril/imaging.py
@@ -1,3 +1,25 @@
+import numpy as np
+from astropy.wcs import WCS
+
+class Grid():
+    def __init__(self,center,fov,scale):
+        self.center, self.fov, self.scale = center, fov, scale
+        self.n = int(fov / scale)
+        self.x = np.linspace(center[0] + fov / 2 - scale / 2, center[0] - fov / 2 + scale / 2, self.n)
+        self.y = np.linspace(center[1] - fov / 2 + scale / 2 , center[1] + fov / 2 - scale / 2, self.n)
+        self.grid = np.meshgrid(self.x, self.y)
+
+        self.wcs = WCS(naxis=2)
+        self.wcs.wcs.crpix = [(self.n+1)/2,(self.n+1)/2]
+        self.wcs.wcs.cdelt = [-self.scale,self.scale]
+        self.wcs.wcs.crval = np.array(self.center)
+        self.wcs.wcs.ctype = ["RA---TAN","DEC--TAN"]
+    
+    def as_2d_array(self):
+        return np.transpose(self.grid,(1,2,0))
+
+    def as_list_of_points(self):
+        return np.reshape(self.as_2d_array(),(-1,2))
 
 class Detector():
     """
